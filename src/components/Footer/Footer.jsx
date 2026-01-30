8cb2a69cb9b1ae192e759a8e20f2008380572425
goldenvertex42 <fabled93@gmail.com>
2026-01-30 13:07:10 -0600

    feat(components): create Header and Footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return(
+    <footer className="siteFooter">
+      <div className="contactInfo">
+        <p><span className="bold">Email:</span> brigidsgeneralstore@notreal.com</p>
+        <p><span className="bold">Phone:</span> 555-555-5555</p>
+        <p><span className="bold">Address:</span> 123 Brigid Drive, Nowhere, USA 12345</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router";
+import styles from "./Header.module.css"
+
+export default function Header({itemsInCart}) {
+  return(
+    <header className={styles.siteHeader}>
+      <div className="storeName">
+        <Link to="/">
+        <h1>Brigid's General Store</h1>
+        </Link>
+      </div>
+      <nav>
+        <Link to="/shop">Shop</Link>
+        <Link to="/mycart">My Cart {itemsInCart}</Link>
+      </nav>
+    </header>
+  );
+}
