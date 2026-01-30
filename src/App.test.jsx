ac7fbeed646039388a522dd8e2cdfac76360b77a
goldenvertex42 <fabled93@gmail.com>
2026-01-30 13:04:02 -0600

    test: test to ensure heading renders properly

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,15 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import App from "./App"
+import { MemoryRouter } from "react-router";
+import App from "./App";
 
 describe("App component", () => {
   it("renders correct heading", () => {
-    render(<App />);
+    render(
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>);
     // using regex with the i flag allows simpler case-insensitive comparison
-    expect(screen.getByRole("heading", { name: "Our First Test" }).textContent).toMatch(/our first test/i);
+    expect(screen.getByRole("heading", { name: "Brigid's General Store" }).textContent).toMatch(/brigid's general store/i);
   });
 });
