b45a97d5e3b4d6ed90c573f34224c3a12e8a07d9
goldenvertex42 <fabled93@gmail.com>
2026-01-30 13:11:03 -0600

    feat(components): create and style QuantityInput component to be used in Shop and MyCart

---
+++ src/components/QuantityInput/QuantityInput.jsx
@@ -0,0 +1,33 @@
+import styles from "./QuantityInput.module.css"
+
+export default function QuantityInput({ quantity, setQuantity }) {
+  function handleSubtractClick() {
+    const newValue = quantity - 1;
+    console.log(newValue);
+    setQuantity(newValue);
+  }
+
+  function handleAddClick() {
+    const newValue = quantity + 1;
+    console.log(newValue);
+    setQuantity(newValue);
+  }
+
+  function validateNumber(e) {
+    const inputValue = e.target.value;
+    if (!isNaN(Number(inputValue))) {
+      setQuantity(inputValue);
+    } else {
+      alert('Please enter a valid number');
+      e.target.value = 1;
+    }
+  }
+  
+  return(
+    <div className={styles.quantityInputContainer}>
+      <button className={styles.subtractBtn} onClick={handleSubtractClick} type="button">-</button>
+      <input className={styles.quantityInput} type="text" value={quantity} name="product-quantity" onChange={validateNumber}/>
+      <button className={styles.addBtn} onClick={handleAddClick} type="button">+</button>
+    </div>
+    );
+}
