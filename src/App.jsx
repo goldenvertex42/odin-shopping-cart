73926b11d83c793e0fe710d4683724e715d8504e
goldenvertex42 <fabled93@gmail.com>
2026-01-30 13:01:59 -0600

    feat: define cartItems state, render outlet router for homepage, shop, and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
-const App = () => <h1>Our First Test</h1>;
+import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router";
+import { useState } from 'react';
 
-export default App;
+export default function App() {
+  const [cartItems, setCartItems] = useState([]);
+  
+  const itemsInCart = cartItems.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.quantity;
+  }, 0);
+  
+  return(
+    <div className="mainContainer">
+      <Header itemsInCart={itemsInCart} />
+      <Outlet context={[cartItems, setCartItems]} />
+      <Footer />
+    </div>
+  );
+}
