f2bae240829f4fb0edb0612e4734cc87ea89f190
goldenvertex42 <fabled93@gmail.com>
2026-01-30 13:05:10 -0600

    feat: create router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import HomePage from './components/HomePage/HomePage.jsx';
+import Shop from './components/Shop/Shop.jsx'
+import MyCart from './components/MyCart/MyCart.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage />},
+      { path: "shop", element: <Shop /> },
+      { path: "mycart", element: <MyCart /> },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
