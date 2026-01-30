64cf743b383f207e83883e96561626ecb236e194
goldenvertex42 <fabled93@gmail.com>
2026-01-30 13:07:35 -0600

    feat(components): create initial HomePage

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return(
+    <main>
+      <div className="welcome">
+        <h2 className="welcomeHeading">Welcome to Brigid's General Store!</h2>
+        <p className="welcomeMessage">
+          Come on in and get warm and cozy and grab something that speaks to you!
+          We have a variety of items from all kinds of places, so try something new!
+        </p>
+      </div>
+    </main>
+  );
+}
