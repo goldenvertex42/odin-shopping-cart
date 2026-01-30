800f49f3afe36d186c8d067b5e600ecabbb73a2e
goldenvertex42 <fabled93@gmail.com>
2026-01-30 13:10:03 -0600

    feat(components): create ProductCard component that will define each card created within Shop component

---
+++ src/components/Shop/ProductCard/ProductCard.jsx
@@ -0,0 +1,20 @@
+import styles from "./ProductCard.module.css"
+import Button from "../../Button/Button";
+import QuantityInput from "../../QuantityInput/QuantityInput";
+import { useState } from "react";
+
+export default function ProductCard({ product, handleAddToCart }) {
+  const [quantityToAdd, setQuantityToAdd] = useState(1);
+  
+  return(
+    <li className={styles.productCard}>
+      <p className={styles.productTitle}>{product.title}</p>
+      <img className={styles.productImage} src={product.image} alt={product.title} />
+      <div className={styles.productInfo}>
+        <p>${Number(product.price).toFixed(2)}</p>
+        <QuantityInput quantity={quantityToAdd} setQuantity={setQuantityToAdd} />
+        <Button onClick={() => handleAddToCart(product, quantityToAdd)}>Add to Cart</Button>
+      </div>
+    </li>
+  );
+}
