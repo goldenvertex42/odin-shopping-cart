112b3f5c0d1603d1170afc48423f946414769583
goldenvertex42 <fabled93@gmail.com>
2026-02-02 11:59:14 -0600

    feat: add function to handle updating the quantity of cart items and component for price display

--- src/components/MyCart/MyCart.jsx
+++ src/components/MyCart/MyCart.jsx
@@ -10,6 +10,42 @@ export default function MyCart() {
     setCartItems(updatedItems);
   }
 
+  const onQuantityChange = (cartItemId, newQuantity) => {
+    const updatedItems = [...cartItems];
+    const itemIndex = updatedItems.findIndex((item) => item.id === cartItemId);
+    updatedItems[itemIndex].quantity = newQuantity;
+    setCartItems(updatedItems);
+  }
+  
+  function PriceDisplay() {
+    const cartSubTotal = cartItems.reduce((accumulator, currentItem) => {
+      return accumulator + (currentItem.price * currentItem.quantity);
+    }, 0);
+    
+    const cartTaxes = cartSubTotal * 0.08;
+
+    const cartTotal = cartSubTotal + cartTaxes;
+
+    const cartTotalQuantity = cartItems.reduce((accumulator, currentItem) => {
+      return accumulator + currentItem.quantity;
+    }, 0);
+
+    return(
+      <div className={styles.priceBox}>
+        <div className={styles.priceLabels}>
+          <p>Subtotal ({cartTotalQuantity} {cartTotalQuantity === 1 ? 'item' : 'items'}):</p>
+          <p>Estimated Tax (8%):</p>
+          <p><span className={styles.bold}>Total:</span></p>
+        </div>
+        <div className={styles.priceNumbers}>
+          <p>${Number(cartSubTotal).toFixed(2)}</p>
+          <p>${Number(cartTaxes).toFixed(2)}</p>
+          <p>${Number(cartTotal).toFixed(2)}</p>
+        </div>
+      </div>
+    );
+  }
+  // FIGURE OUT HOW TO UPDATE THE ITEMS IN CART WHEN QUANTITY UPDATES
   if (cartItems.length === 0) return <h2>There's nothing in your cart yet! Add some items from the shop!</h2>
   
   return (
@@ -19,9 +55,12 @@ export default function MyCart() {
           {cartItems.map((cartItem) => (
             <CartCard key={cartItem.id}
                       cartItem={cartItem} 
-                      handleRemoveFromCart={removeFromCart} />
+                      handleRemoveFromCart={removeFromCart} 
+                      onQuantityChange={(newQuantity) => onQuantityChange(cartItem.id, newQuantity)} />
           ))}
         </ul>
+        <hr />
+        <PriceDisplay />
       </div>
     </main>
   );
