8b0b792e54d1628ef36567b99eeab77aaf49a424
goldenvertex42 <fabled93@gmail.com>
2026-02-02 11:57:56 -0600

    fix: delete unnecessary state for calculatable quantity

--- src/components/MyCart/CartCard/CartCard.jsx
+++ src/components/MyCart/CartCard/CartCard.jsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
 import QuantityInput from "../../QuantityInput/QuantityInput";
 import Button from "../../Button/Button";
-import styles from "./CartCard.module.css"
+import styles from "./CartCard.module.css";
 
-export default function CartCard({ cartItem, handleRemoveFromCart }) {
-  const [quantity, setQuantity] = useState(cartItem.quantity);
-
-  const price = cartItem.price * quantity;
-
+export default function CartCard({ cartItem, handleRemoveFromCart, onQuantityChange }) {
   return(
     <li className={styles.cartCard}>
       <img className={styles.cartItemImage} src={cartItem.image} alt={cartItem.title} />
       <p className={styles.cartItemTitle}>{cartItem.title}</p>
       <div className={styles.cartItemInfo}>
-        <QuantityInput quantity={quantity} setQuantity={setQuantity} />
-        <p>${Number(price).toFixed(2)}</p>
+        <QuantityInput quantity={cartItem.quantity} setQuantity={(newQuantity) => onQuantityChange(newQuantity)} />
+        <p>${Number((cartItem.price * cartItem.quantity)).toFixed(2)}</p>
         <Button className={styles.removeBtn} onClick={() => handleRemoveFromCart(cartItem.id)}>Remove</Button>
       </div>
     </li>
