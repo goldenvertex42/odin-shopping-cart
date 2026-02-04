ea34586159c3644cbf9e356ad8b842d6cc2cff15
goldenvertex42 <fabled93@gmail.com>
2026-02-04 12:01:03 -0600

    fix: ensure quantity cannot go below 1

--- src/components/QuantityInput/QuantityInput.jsx
+++ src/components/QuantityInput/QuantityInput.jsx
@@ -2,6 +2,9 @@ import styles from "./QuantityInput.module.css"
 
 export default function QuantityInput({ quantity, setQuantity }) {
   function handleSubtractClick() {
+    if (quantity === 1) {
+      return;
+    }
     const newValue = quantity - 1;
     setQuantity(newValue);
   }
@@ -13,7 +16,7 @@ export default function QuantityInput({ quantity, setQuantity }) {
 
   function validateNumber(e) {
     const inputValue = e.target.value;
-    if (!isNaN(Number(inputValue))) {
+    if (!isNaN(Number(inputValue)) && Number(inputValue) !== 0) {
       setQuantity(Number(inputValue));
     } else {
       alert('Please enter a valid number');
@@ -24,6 +27,7 @@ export default function QuantityInput({ quantity, setQuantity }) {
   return(
     <div className={styles.quantityInputContainer}>
       <button className={styles.subtractBtn} onClick={handleSubtractClick} type="button">-</button>
+      <label htmlFor="product-quantity" className={styles.visuallyHidden}>Quantity:</label>
       <input className={styles.quantityInput} type="text" value={quantity} name="product-quantity" onChange={validateNumber}/>
       <button className={styles.addBtn} onClick={handleAddClick} type="button">+</button>
     </div>
