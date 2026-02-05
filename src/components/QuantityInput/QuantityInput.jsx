0d49f9e07cbc169b4a42b6aa0c6251039332f4c3
goldenvertex42 <fabled93@gmail.com>
2026-02-05 11:26:54 -0600

    fix: add unique identifier to each quantity input rendered simultaneously

--- src/components/QuantityInput/QuantityInput.jsx
+++ src/components/QuantityInput/QuantityInput.jsx
@@ -1,6 +1,10 @@
 import styles from "./QuantityInput.module.css"
 
+let idCounter = 0;
+
 export default function QuantityInput({ quantity, setQuantity }) {
+  const inputId = `product-quantity-${idCounter++}`;
+  
   function handleSubtractClick() {
     if (quantity === 1) {
       return;
@@ -18,6 +22,8 @@ export default function QuantityInput({ quantity, setQuantity }) {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue)) && Number(inputValue) !== 0) {
       setQuantity(Number(inputValue));
+    } else if (Number(inputValue) === 0) {
+      setQuantity(1);
     } else {
       alert('Please enter a valid number');
       e.target.value = 1;
@@ -27,8 +33,8 @@ export default function QuantityInput({ quantity, setQuantity }) {
   return(
     <div className={styles.quantityInputContainer}>
       <button className={styles.subtractBtn} onClick={handleSubtractClick} type="button">-</button>
-      <label htmlFor="product-quantity" className={styles.visuallyHidden}>Quantity:</label>
-      <input className={styles.quantityInput} type="text" value={quantity} name="product-quantity" onChange={validateNumber}/>
+      <label htmlFor={inputId} className={styles.visuallyHidden}>Quantity:</label>
+      <input className={styles.quantityInput} type="text" placeholder={quantity} name="product-quantity" id={inputId} onChange={validateNumber}/>
       <button className={styles.addBtn} onClick={handleAddClick} type="button">+</button>
     </div>
     );
